29966483c3d596d4eed5cd9db50b8cf64985500f
rohan <rohan_dubey@pokerstarsint.com>
2025-07-05 21:50:27 +0530

    Adding logic to kill any process on port 8080
    Adding logic to kill any process on port 8080 before running

--- cmd/goxstream/main.go
+++ cmd/goxstream/main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
-    "fmt"
-    "goxstream/internal/api"
+	"fmt"
+	"goxstream/internal/api"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"syscall"
 )
 
 func main() {
-    fmt.Println("GoXStream REST API running on :8080")
-    if err := api.StartAPIServer(":8080"); err != nil {
-        panic(err)
-    }
+	killProcess()
+	fmt.Println("GoXStream REST API running on :8080")
+	if err := api.StartAPIServer(":8080"); err != nil {
+		panic(err)
+	}
+}
+
+func killProcess() {
+	out, err := exec.Command("lsof", "-t", "-i:8080").Output()
+	if err != nil || len(out) == 0 {
+		return // Nothing running or lsof not available
+	}
+	pids := strings.Fields(string(out))
+	for _, pidStr := range pids {
+		pid, _ := strconv.Atoi(pidStr)
+		proc, _ := os.FindProcess(pid)
+		proc.Signal(syscall.SIGKILL)
+	}
 }
